a2622eb10223066ed63ae52f06d48356054b4838
karust <karust13@gmail.com>
2019-03-29 12:37:36 -0700

    check mbr

--- readmbr.py
+++ readmbr.py
@@ -1,9 +1,25 @@
 # https://github.com/hamptus/pyMBR/blob/master/mbr.py
 # https://stackoverflow.com/questions/41411675/reading-boot-sector-on-windows
-
+import hashlib
 import os
-disk_fd = os.open( r"\\.\PhysicalDrive0", os.O_RDONLY | os.O_BINARY)
-data = os.read(disk_fd, 512)
-os.close(disk_fd)
+from binascii import hexlify, unhexlify
 
-print(data)
+
+
+if __name__ == "__main__":
+    m = hashlib.sha256()
+
+    disk_fd = os.open( r"\\.\PhysicalDrive0", os.O_RDONLY | os.O_BINARY)
+    data = os.read(disk_fd, 512)
+    os.close(disk_fd)
+
+    m.update(data)
+    h = m.digest()
+
+    coolHash = b'4f198add422223d6a067f7cbdf3a99e28fbae4c6926112cc0a5449e8d9c8da12'
+    
+    if h == unhexlify(coolHash):
+        print("Cool")
+    else:
+        print("Your MBR is changed by H@Cker")
+    print(hexlify(h))
